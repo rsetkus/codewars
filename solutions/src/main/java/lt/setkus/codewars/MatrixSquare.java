12e95455c1a801939ec4abb92338828f3a3feb90
Robertas Setkus <robertas.setkus@gmail.com>
2018-05-04 21:48:34 +0100

    Solved matrix square up kata.

--- solutions/src/main/java/lt/setkus/codewars/MatrixSquare.java
+++ solutions/src/main/java/lt/setkus/codewars/MatrixSquare.java
@@ -4,7 +4,19 @@ package lt.setkus.codewars;
  * https://www.codewars.com/kata/matrix-square-up-b
  */
 public class MatrixSquare {
-    public static char[][] matrixSquareUp(int i) {
-        return new char[i][i];
+    public static String[][] matrixSquareUp(int i) {
+        String[][] matrix = new String[i][i];
+        for (int row = 0; row < matrix.length; row++) {
+            int counter = 1;
+            for (int column = matrix[row].length - 1; column >= 0; column--) {
+                if (counter <= row + 1) {
+                    matrix[row][column] = String.valueOf(counter);
+                    counter++;
+                } else {
+                    matrix[row][column] = "x";
+                }
+            }
+        }
+        return matrix;
     }
 }
